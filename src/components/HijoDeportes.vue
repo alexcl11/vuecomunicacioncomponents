f681331e2961f8277aaac7274160835d9140a880
alexcl11 <aleecanovaslopez@gmail.com>
2025-10-27 14:15:24 +0100

    up

---
+++ src/components/HijoDeportes.vue
@@ -0,0 +1,10 @@
+<template>
+    <h1 style="color: blue">Hijo Deportes</h1>
+    <p>Me gusta el {{nombreDeporte}}</p>
+</template>
+<script>
+    export default{
+        name: "HijoDeportes",
+        props: ["nombreDeporte"]
+    }
+</script>
---
+++ src/components/PadreDeportes.vue
@@ -0,0 +1,24 @@
+<template>
+    <h1 style="color: red">Padre Deportes</h1>
+    <ul>
+        <li v-for="deporte in deportes" :key="deporte">
+            <HijoDeportes :nombreDeporte="deporte"/>
+        </li>        
+    </ul>
+    
+</template>
+<script>
+    import HijoDeportes from './HijoDeportes.vue'
+
+    export default{
+        name: "PadreDeportes",
+        components: {
+            HijoDeportes
+        },
+        data(){
+            return{
+                deportes: ["Curling", "Peonza", "Canicas", "Bolos"]
+            }
+        }
+    }
+</script>
